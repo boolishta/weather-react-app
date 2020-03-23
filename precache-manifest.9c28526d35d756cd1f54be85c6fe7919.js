ed88f98779a5e7e0e65cba4d8936fc42f62f4a1d
boolishta <batr.fly@mail.ru>
2020-03-23 11:42:34 +0300

    Updates

---
+++ precache-manifest.9c28526d35d756cd1f54be85c6fe7919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98ef865af75991dc2abbdec9f845b3d",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "c2f79156eecf9736a0d2",
+    "url": "/weather-react-app/static/css/main.619b8437.chunk.css"
+  },
+  {
+    "revision": "8da5420a6b68b7395235",
+    "url": "/weather-react-app/static/js/2.74ccec93.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weather-react-app/static/js/2.74ccec93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f79156eecf9736a0d2",
+    "url": "/weather-react-app/static/js/main.6b7747b2.chunk.js"
+  },
+  {
+    "revision": "55e1ab2d6a2b51f320a1",
+    "url": "/weather-react-app/static/js/runtime-main.a82fa206.js"
+  }
+]);
